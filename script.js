556644ed72493f8304412f702e6f9fddeb306846
cjd <carljduff@gmail.com>
2025-05-07 21:21:28 -0400

    Added functionality to create a todo. A todo can be created with its label, checkbox, and delete button.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World")
+const inputBox = document.getElementById("input-todo");
+const todoContainer = document.getElementById("todo-container");
+const addTodoButton = document.getElementById("add-todo-button");
+
+addTodoButton.addEventListener("click", createTodo)
+
+function createTodo() {
+    if (inputBox.value == "") return;
+
+    let container = document.createElement("div");
+    let todoText = document.createElement("p");
+    todoText.innerText = inputBox.value;
+    container.appendChild(todoText);
+    todoContainer.append(container);
+
+    let checkTodo = document.createElement("input");
+    checkTodo.setAttribute("type", "checkbox");
+    container.appendChild(checkTodo);
+
+    let deleteTodo = document.createElement("button");
+    deleteTodo.innerText = "x";
+    container.appendChild(deleteTodo)
+
+    inputBox.value = ""
+
+}
